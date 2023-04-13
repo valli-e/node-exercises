844c360cb429102a0ae293d3f01c482e70bb1939
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-04-13 09:12:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// Exercise 3 - Create and use CommonJS modules
+
+// Create a script that uses module.exports to export a function.
+// Create another script that uses require() to import the function and then calls it.
+
+
+
+function sayHello(message) {
+    console.log(message)
+}
+
+export default sayHello;
