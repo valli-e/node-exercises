556dc481704febdcda61e68088377ee0e0643500
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-04-13 09:02:47 +0200

    Add files via upload

---
+++ exercise-2.js
@@ -0,0 +1,7 @@
+// Exercise 2: Explore with the Node.js REPL
+
+// Use the Node.js REPL to list the methods provided by the Node.js core crypto module. Use one of these methods to generate a random ID.
+
+const crypto = require('crypto');
+const id = crypto.randomBytes(16).toString('hex');
+ console.log(id);
