59933040862d9b78a12a368743ebcb16bf76c5c7
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-04-26 12:48:43 +0200

    promise chain created

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+luckyDraw('Joe')
+  .then(result => {
+    console.log(result);
+    return luckyDraw('Caroline');
+  })
+  .then(result => {
+    console.log(result);
+    return luckyDraw('Sabrina');
+  })
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.error(error);
+  });
