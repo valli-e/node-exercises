be813e3fb81f056ee633598ebd11f4facf74ab75
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-05-17 12:46:36 +0200

    Create server.ts

---
+++ server.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import pgPromise from "pg-promise"; 
+import Joi from "joi";
+import { getAll, getOneById, create, updatedById, deleteById } from "./controllers/planets"; 
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(morgan("dev"));
+app.use(express.json());
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: number;
+      DATABASE_URL: string;
+    }
+  }
+}
+
+app.get('/api/planets', getAll); 
+app.get('/api/planets/:id', getOneById); 
+app.post('/api/planets', create); 
+app.put('/api/planets/:id', updatedById); 
+app.delete("/api/planets/:id", deleteById); 
+
+app.listen(port, () => {
+  console.log(`example app running on port http://localhost:${port}`);
+});
