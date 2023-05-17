2f8933993465f0c1946b6602b1b0829699c18f29
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-05-17 12:54:17 +0200

    Create users.ts

---
+++ users.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken"; 
+import { db } from "./../db.js"; 
+import * as dotenv from "dotenv";
+dotenv.config(); 
+
+interface AuthRequest extends Request {
+    user?: any;
+  }
+
+const logIn = async (req: Request, res: Response) => {
+    const { username, password } = req.body
+
+    const user = await db.one(`SELECT * FROM users WHERE username=$1`, username)
+    if(user && user.password === password) {
+        const payload = {
+            id: user.id, 
+            username,
+        }; 
+        const { SECRET = " " } = process.env
+        const token = jwt.sign(payload,SECRET); 
+        await db.none(`UPDATE users SET token=$2 WHERE id=$1`,[user.id, token] )
+        res.status(200).json({id: user.id, username, token})
+
+    } else {
+        res.status(400).json({ msg: "username or password incorrect."})
+
+    }
+
+}
+
+const signUp = async (req: Request, res: Response) => {
+    const { username, password} = req.body;
+    const user = await db.oneOrNone(`SELECT * FROM users WHERE username=$1`, username);
+    if(user) {
+        res.status(409).json({msg: "username already in use"})
+    }else {
+        await db.one(`INSERT INTO users (username, password) VALUES($1, $2) RETURNING id`, [username, password]);
+    
+    res.status(201).json ({  msg: "user created successfully"})
+}
+}
+
+
+export {logIn, signUp }
