8c0cc381813a9ea34dc66f232471ca7ea67e1705
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-05-17 12:48:33 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import "express-async-errors";
+import multer from "multer"; 
 import morgan from "morgan";
 import pgPromise from "pg-promise"; 
 import Joi from "joi";
-import { getAll, getOneById, create, updatedById, deleteById } from "./controllers/planets"; 
+import { getAll, getOneById, create, updatedById, deleteById, createImage} from "./controllers/planets"; 
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -21,12 +22,24 @@ declare global {
     }
   }
 }
+const storage = multer.diskStorage ({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads")
+  
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  },
+})
 
+const upload = multer({ storage })
 app.get('/api/planets', getAll); 
 app.get('/api/planets/:id', getOneById); 
 app.post('/api/planets', create); 
 app.put('/api/planets/:id', updatedById); 
 app.delete("/api/planets/:id", deleteById); 
+
+app.post("/api/planets/:id/image", upload.single("image"), createImage)
 
 app.listen(port, () => {
   console.log(`example app running on port http://localhost:${port}`);
