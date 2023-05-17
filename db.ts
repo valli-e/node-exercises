5ffabea074df882fd48c45fe5bd2bb9ff6a39fef
Valentyna Hnatyuk <114984511+valli-e@users.noreply.github.com>
2023-05-17 12:52:41 +0200

    Create db.ts

---
+++ db.ts
@@ -0,0 +1,36 @@
+import pgPromise from "pg-promise"; 
+const { DATABASE_URL } = process.env;
+
+ const db = pgPromise()(DATABASE_URL);
+
+const setupDb = async () => {
+    await db.none(`
+      DROP TABLE IF EXISTS planets;
+
+      CREATE TABLE planets (
+          id SERIAL NOT NULL PRIMARY KEY,
+          name TEXT NOT NULL
+          image TEXT
+      );
+      DROP TABLE IF EXISTS users; 
+
+      CREATE TABLE users (
+        id SERIAL NOT NULL PRIMARY KEY, 
+        username TEXT NOT NULL, 
+        password TEXT NOT NULL, 
+        token TEXT, 
+      
+      )
+
+    `);
+
+ 
+      await db.none (`INSERT INTO planets (name) VALUES ('Earth')`);
+      await db.none (`INSERT INTO planets (name) VALUES ('Mars')`);
+      await db.none (`INSERT INTO (username, password) VALUES ('dummy', 'dummy')`);
+  
+  };
+  
+  setupDb();
+
+  export { db }
